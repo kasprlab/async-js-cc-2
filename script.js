731ac38dcefaea24d5293f4ddeb250a99a06e89c
Kay Putra <kaspr.works@gmail.com>
2022-04-24 21:54:59 +1000

    completed task 6 - 8. Challenge completed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 console.log('async coding challenge 2')
 
+
+
 const wait = function(seconds){
   return new Promise(function(resolve){
     setTimeout(resolve, seconds * 1000)
@@ -28,11 +30,23 @@ const createImage = function(imgPath) {
   
 }
 
+let currentImg
+
 createImage('img/img-1.jpg')
 .then(img => {
+  currentImg = img
   console.log('Image 1 loaded')
   return(wait(2))
 })
-.then(() => imgContainer.style.display = 'none')
+.then(() => {
+  currentImg.style.display = 'none'
+  return createImage('img/img-2.jpg')
+})
+.then(img => {
+  currentImg = img
+  console.log('Image 2 loaded')
+  return(wait(2))
+})
+.then(() => currentImg.style.display = 'none')
 .catch(err => console.error(err))
 
